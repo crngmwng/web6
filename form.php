f193ebe975d25e955325c491544a58c12ef2637e
Александра Щупоченко <52908598+crngmwng@users.noreply.github.com>
2022-05-04 17:16:40 +0300

    Update form.php

--- form.php
+++ form.php
@@ -26,7 +26,59 @@
             document.getElementById("year").appendChild(options).innerHTML = year;
           }
         </script>
-        
+        Пол:
+        <label>
+            <input type="radio" 
+                   name="radio-group-1" value="m" <?php if($values['radio-group-1']=="m") {print 'checked';} ?> />
+            Муж
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-1" value="f"  <?php if($values['radio-group-1']=="f") {print 'checked';} ?>/>
+            Жен
+        </label><br />
+        Количество конечностей: <br />
+        <label>
+            <input type="radio" 
+                   name="radio-group-2" value="1" 
+                   <?php if($values['radio-group-2']=="1") {print 'checked';} ?>/>
+            1
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-2" value="2"
+                   <?php if($values['radio-group-2']=="2") {print 'checked';} ?>/>
+            2
+        </label>
+        <label>
+            <input type="radio" 
+                   name="radio-group-2" value="3"  
+                   <?php if($values['radio-group-2']=="3") {print 'checked';} ?>/>
+            3
+        </label>
+        <label>
+            <input type="radio"
+                   name="radio-group-2" value="4"
+                   <?php if($values['radio-group-2']=="4") {print 'checked';} ?>/>
+            4
+        </label><br />
+        <label>
+            Сверхспособности:
+            <br />
+            <select name="power"
+                    multiple="multiple">
+                <option value="immortality">Бессмертие</option>
+                <option value="pass_thr_walls">Прохождение сквозь стены</option>
+                <option value="levitation" >Левитация</option>
+            </select>
+        </label><br />
+        <label>
+           Биография:<br />
+            <textarea name="bio">   <?php print $values['bio']; ?> </textarea>
+        </label><br />
+        <label><input type="checkbox"  name="check-1" <?php if($values['check-1']==TRUE){print 'checked';} ?> />
+            С контрактом ознакомлен
+        </label><br />
         
          <input type="submit" value="ok"  />
     </form>
