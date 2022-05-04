4e21ad1253bb45b0ca73a0c0f791a362ebfb6628
Александра Щупоченко <52908598+crngmwng@users.noreply.github.com>
2022-05-04 12:37:27 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -24,8 +24,24 @@ print('Вы успешно авторизовались и видите защи
 // *********
 // Здесь нужно прочитать отправленные ранее пользователями данные и вывести в таблицу.
 // Реализовать просмотр и удаление всех данных.
+  $user = 'u47590';
+$pass = '3205407';
+$db = new PDO('mysql:host=localhost;dbname=u47590', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+try {
+ $login = $_SESSION['login'];
+ $stmt = $db->prepare("SELECT `login`, `password`, `name` FROM `app`");
+    $stmt->execute();
+    foreach ($stmt as $row) {
+      "<tr><td>{$row['login']}</td><td>{$row['password']}</td><td>{$row['name']} </td></tr>";
+      }
+}
+   
+catch(PDOException $e){
+  print('Error : ' . $e->getMessage());
+  exit();
+}
 
-$user = 'u47590';
+/*$user = 'u47590';
 $pass = '3205407';
 $host = 'localhost';
 $db_name='u47590';
@@ -44,7 +60,6 @@ $link = mysqli_connect($host, $user, $pass, $db_name);
     }
   ?>
 </table>
-</body>
-</html>
 
+*/
 // *********
